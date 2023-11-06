56a0faeecd836fcfb70394ba31239c8059fa32ef
George <george_rtaylor@yahoo.com>
2023-11-05 21:52:07 -0600

    commit

---
+++ week-3/sports-app/index.js
@@ -0,0 +1,31 @@
+// Import the team manger module from the team-manager.js file
+const TeamManager = require('./team-manager');
+
+//Get a an array of team objects from the team-manager.js file.
+let teams = TeamManager.getTeams();
+
+//Display teh contents of the teams array.
+for (let team of teams){
+    console.log(TeamManager.displayTeam(teams));
+}
+
+const Alabama = TeamManager.getTeam('Alabama');
+
+
+const LSU = TeamManager.getTeam('LSU');
+
+
+const Georgia = TeamManager.getTeam('Georgia');
+
+
+const Texas = TeamManager.getTeam('Texas');
+
+
+const USC = TeamManager.getTeam('USC');
+
+console.log(TeamManager.displayTeam(Alabama));
+console.log(TeamManager.displayTeam(LSU));
+console.log(TeamManager.displayTeam(Georgia));
+console.log(TeamManager.displayTeam(Texas));
+console.log(TeamManager.displayTeam(USC));
+
