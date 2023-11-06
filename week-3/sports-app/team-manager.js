83496ee1b14294500c11472872d9328593bc17f8
George <george_rtaylor@yahoo.com>
2023-11-05 21:27:38 -0600

    commit

---
+++ week-3/sports-app/team-manager.js
@@ -0,0 +1,34 @@
+// Imports the Team class from team.js file 
+const Team = require('./team-manager');
+
+//Creates an array of total of 5 teams an assigns team values
+
+let teams = [
+    new Team('Alabama', 'Crimson Tide', '54'),
+    new Team('LSU', 'Tigers', '54'),
+    new Team('Georgia', 'Bulldogs', '54'),
+    new Team('Texas', 'Longhorns', '54'),
+    new Team('USC', 'Trojans', '54'),
+]
+
+//Create a function name getTeams that returns
+// and array of Team description.
+module.exports.getTeams = function() {
+    return teams;
+} 
+
+//Creates a function named getTeam with a single parameter for name. 
+//Return a single team object using JavaScript's built-in find()function. 
+module.exports.getTeam = function(name) {
+    for (let team of Teams) {
+        if (team.name === name){
+           return team 
+        }
+    }
+}
+
+//Creates a function name displayTeam with a single parameter for team. 
+//Returns a formatted string using the team object properties. 
+module.exports.displayTeam = function(team) {
+    return 'Name:' + team.name + '\nMascot:' + team.mascot + '\nPlayers Count:' + team.players + '\n';
+}
--- week-3/sports-app/team.js
+++ week-3/sports-app/team.js
@@ -1,34 +1,16 @@
-// Imports the Team class from team.js file 
-const Team = require('./team');
-
-//Creates an array of total of 5 teams an assigns team values
-
-let teams = [
-    new Team('Alabama', 'Crimson Tide', '54'),
-    new Team('LSU', 'Tigers', '54'),
-    new Team('Georgia', 'Bulldogs', '54'),
-    new Team('Texas', 'Longhorns', '54'),
-    new Team('USC', 'Trojans', '54'),
-]
-
-//Create a function name getTeams that returns
-// and array of Team description.
-module.exports.getTeams = function() {
-    return teams;
-} 
-
-//Creates a function named getTeam with a single parameter for name. 
-//Return a single team object using JavaScript's built-in find()function. 
-module.exports.getTeam = function(name) {
-    for (let team of Teams) {
-        if (team.name === name){
-           return team 
-        }
+/**
+ * Creates a Team class with properties for team name, mascot, and player count.
+ * Properties are initialized in the classes constructor.
+ * Class is exported using Node's module system
+ */
+class Team {
+    constructor(name, mascot, players){
+        this.name = name;
+        this.mascot = mascot;
+        this.players = players;
     }
 }
 
-//Creates a function name displayTeam with a single parameter for team. 
-//Returns a formatted string using the team object properties. 
-module.exports.displayTeam = function(team) {
-    return 'Name:' + Team.name + '\nMascot:' + team.mascot + '\nPlayers Count:' + team.players + '\n';
-}
+// Uses Nodes module system to export the Team class
+// This makes the Team class accessible in other files.
+module.exports = Team;
